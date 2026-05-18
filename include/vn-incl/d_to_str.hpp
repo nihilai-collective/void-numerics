d7dee2af884fc8c0af60a88ec9952348b78b0773
RealTimeChris <40668522+RealTimeChris@users.noreply.github.com>
2026-05-18 11:02:30 -0400

    Initial commit

---
+++ include/vn-incl/d_to_str.hpp
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2026 Nihilai Collective Corp
+// vn-incl/d_to_str.hpp
+
+#pragma once
+
+#include <vn-incl/utility.hpp>
+#include <vn-incl/i_to_str.hpp>
+
+namespace vn {
+
+	template<detail::float_types v_type>
+	VN_FORCE_INLINE std::to_chars_result to_chars(char* first, char* last, v_type value, std::chars_format format = std::chars_format::fixed) noexcept {
+		auto r = std::to_chars(first, last, value, format);
+		return { r.ptr, r.ec };
+	}
+
+}
